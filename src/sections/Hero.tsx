a58087f67bedea8d973e1dae8984f609808a29de
OmarSalem123 <omar123789ali@gmail.com>
2024-12-22 11:13:40 +0200

    compress image

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,14 +3,14 @@ import Button from "../components/Button";
 
 const Hero = () => {
   return (
-    <section className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32">
+    <section className="relative pb-40 pt-60 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32">
       <Element name="hero">
         <div className="container">
           <div className="relative z-2 max-w-512 max-lg:max-w-388">
-            <div className="caption small-2 uppercase text-p3">
+            <div className="uppercase caption small-2 text-p3">
               video Editing
             </div>
-            <h1 className="mb-6 h1 text-p4 uppercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
+            <h1 className="mb-6 uppercase h1 text-p4 max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
               Amazingly simple
             </h1>
             <p className="max-w-440 mb-14 body-1 max-md:mb-10">
@@ -23,7 +23,7 @@ const Hero = () => {
           </div>
           <div className="absolute -top-32 left-[calc(50%-340px)] w-[1230px] pointer-events-none hero-img_res">
             <img
-              src="/images/hero.png"
+              src="/images/hero.webp"
               className="size-1230 max-lg:h-auto"
               alt="hero"
             />
