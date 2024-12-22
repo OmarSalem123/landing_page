76fd4c9e1336be8db20e3a2499e962406053ba7b
OmarSalem123 <omar123789ali@gmail.com>
2024-12-22 10:46:54 +0200

    npm run build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "127.0.0.1", // Use IPv4 instead of IPv6 (::1)
+    port: 3000,
+  },
   plugins: [react()],
-})
+});
